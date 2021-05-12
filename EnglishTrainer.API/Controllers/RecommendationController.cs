27b5b65851f58b834e10e3a643f6e0a221ca5537
ilyaa <ilyaaxenov1999@gmail.com>
2021-05-13 02:12:34 +0300

    recommendation controller added

---
+++ EnglishTrainer.API/Controllers/RecommendationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EnglishTrainer.Core.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishTrainer.API.Controllers
+{
+    [Route("recommendations")]
+    public class RecommendationController: ControllerBase
+    {
+        private readonly IRecommendationService _recommendationService;
+
+        public RecommendationController(IRecommendationService recommendationService)
+        {
+            _recommendationService = recommendationService;
+        }
+
+        [HttpGet]
+        [Route("{userId}")]
+        public IActionResult GetRecommendationsForUser([FromRoute] Guid userId)
+        {
+            var recommendedSets = _recommendationService.GetByUserId(userId);
+
+            var response = new
+            {
+                RecommendedSets = recommendedSets
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult GenerateRecommendations()
+        {
+            _recommendationService.Generate();
+            
+            return Ok();
+        }
+    }
+}
